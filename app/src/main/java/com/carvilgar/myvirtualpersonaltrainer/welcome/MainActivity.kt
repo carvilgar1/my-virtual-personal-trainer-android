3503463b6c97496c87915320e27e13fdc1bd2b5d
carvilgar1 <villa.carlosjesus@gmail.com>
2022-10-25 09:09:14 +0200

    fix(Welcome): Renamed SingInActivity to SignInActivity that was causing a Exception

--- app/src/main/java/com/carvilgar/myvirtualpersonaltrainer/welcome/MainActivity.kt
+++ app/src/main/java/com/carvilgar/myvirtualpersonaltrainer/welcome/MainActivity.kt
@@ -5,17 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.carvilgar.myvirtualpersonaltrainer.R
-import com.carvilgar.myvirtualpersonaltrainer.singin.SingInActivity
+import com.carvilgar.myvirtualpersonaltrainer.sign_in.SignInActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //create a button that goes to the sing in activity
         val button = findViewById<Button>(R.id.welcome_sign_in_button)
         button.setOnClickListener{
-            val intent = Intent(this, SingInActivity::class.java)
+            val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
         }
     }
