bcb103b6e82c3e72661622872c5953ee77679587
carvilgar1 <villa.carlosjesus@gmail.com>
2022-10-30 16:14:36 -0700

    perf(Global): Improved visibility in dark mode
    Close #3

--- app/src/main/java/com/carvilgar/myvirtualpersonaltrainer/sign_in/SignInActivity.kt
+++ app/src/main/java/com/carvilgar/myvirtualpersonaltrainer/sign_in/SignInActivity.kt
@@ -2,7 +2,7 @@ package com.carvilgar.myvirtualpersonaltrainer.sign_in
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import com.carvilgar.myvirtualpersonaltrainer.R
 import com.carvilgar.validation.ValidationError
@@ -12,9 +12,15 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        //signIp button click listener
+        val signUpButton = findViewById<Button>(R.id.btn_sign_in)
+        signUpButton.setOnClickListener {
+            signIn()
+        }
     }
 
-    fun signIn(view: View) {
+    private fun signIn() {
         val emailEditText = findViewById<EditText>(R.id.et_email_sign_in)
         val passwordEditText = findViewById<EditText>(R.id.et_pass_sign_in)
         val errors = ValidationError<Any?>()
